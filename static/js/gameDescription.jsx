c76fc2c482251fa8ca454bb0a27a002b47360aa9
Eyalant <eyal.lantz@outlook.com>
2023-05-11 00:23:33 +0300

    initial

--- static/js/gameDescription.jsx
+++
@@ -1,8 +0,0 @@
-function GameDescription({ release_info, description }) {
-    return (
-        <>
-            <Card.Text className="fw-bold">{release_info}</Card.Text>
-            <Card.Text>{description}</Card.Text>
-        </>
-    )
-}
